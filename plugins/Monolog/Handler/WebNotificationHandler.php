c3282315b1d281ad570123962263bcd8b94a5c68
unclemusclez <unclemusclez@gmail.com>
2025-04-10 21:22:20 -0400

    public function isHandling(array $record):void for Fatal error: Declaration of Piwik\Plugins\Monolog\Handler\WebNotificationHandler::isHandling(array $record) must be compatible with Monolog\Handler\AbstractHandler::isHandling(array $record): bool in plugins/Monolog/Handler/WebNotificationHandler.php on line 26

--- plugins/Monolog/Handler/WebNotificationHandler.php
+++ plugins/Monolog/Handler/WebNotificationHandler.php
@@ -23,7 +23,7 @@ class WebNotificationHandler extends AbstractProcessingHandler
 {
     public const MAX_NOTIFICATION_MESSAGE_LENGTH = 512;
 
-    public function isHandling(array $record)
+    public function isHandling(array $record):void
     {
         if (!empty($record['context']['ignoreInScreenWriter'])) {
             return false;
